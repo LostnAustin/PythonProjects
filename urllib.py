30a05ebf9f26fc95a330a18e4abce01b2cf29070
Chad Lengefeld <36778876+LostnAustin@users.noreply.github.com>
2023-05-04 01:18:33 -0500

    Update urllib.py

--- urllib.py
+++ urllib.py
@@ -1,5 +1,7 @@
 import urllib.request, urllliv.parse, urllib.error
 
+#grabbing html from the web
+
 fhand = urllib.request.urlopen('http:/www.dr-chuck.com/page1.html')
 for line in fhand:
   print(line.decode().strip())
